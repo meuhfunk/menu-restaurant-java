19c5a787653fce7fc2adeedbf7ffac061950e16a
croc <leymarien@gmail.com>
2018-08-29 16:40:03 +0200

    Test Driven Development

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -36,5 +38,13 @@ public class Order {
         }
 
     }
-
+    /**
+     * Run asking process for a menu.
+     */
+    public void runMenu() {
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
+    }
 }
--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -74,5 +76,15 @@ public class OrderTest {
 
         assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", outContent.toString().replace("\r\n", "\n"));
 
+    }
+
+    @Test
+    public void Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+        System.setIn(new ByteArrayInputStream("1\n".getBytes()));
+        order = new Order();
+        order.runMenu();
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals(output.endsWith("Vous avez choisi comme menu : poulet\n"), true);
+        assertEquals(output.length() > "Vous avez choisi comme menu : poulet\n".length(), true);
     }
 }
